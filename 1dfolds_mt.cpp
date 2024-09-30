2fd714fa4731198bcb3105eb5761f025f0746edc
Chris Rycroft <chr@seas.harvard.edu>
2024-09-30 10:57:21 -0500

    omp parallel for example.

--- 1dfolds_mt.cpp
+++ 1dfolds_mt.cpp
@@ -11,10 +11,10 @@ int main() {
     
     //fs.log_fixedn();
     int n = 160;
-#pragma omp parallel
-    {
+#pragma omp parallel for
+    for(int j=0;j<16;j++) {
         int max_t=omp_get_max_threads(),th=omp_get_thread_num();
-        printf("Hello from %d of %d threads\n",th,max_t);
+        printf("j=%d is processed by %d of %d threads\n",th,max_t);
 
         //folds_stats fs; 
         //fs.segdens(n);
