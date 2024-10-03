91f6539f3324f7adcaeb16519f17de03085dc02e
annsbigelow <annsbigelow@gmail.com>
2024-10-03 09:30:02 -0500

    Shasta test n40

--- definitions.cpp
+++ definitions.cpp
@@ -61,7 +61,7 @@ void folds_stats::segdens(int &numplaces) {
     sprintf(buf,"NormSegDens_testomp%d.txt",th);
     densData.open(buf);*/
 
-    densData.open("NormSegDens_ompN60.txt");
+    densData.open("NormSegDens_omptest.txt");
     for (int j = 0; j < numplaces; j++) {
         //domavg[j] /= instance;
         normx[j] = (domain[j] - lo) / (hi - lo); // normalize x-axis
