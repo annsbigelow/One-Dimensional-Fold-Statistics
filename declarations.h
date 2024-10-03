b3ac541e037e3dba79d92de504f50ac2f5d3b70f
annsbigelow <annsbigelow@gmail.com>
2024-10-03 08:30:22 -0500

    Shasta segdens n=42

--- declarations.h
+++ declarations.h
@@ -21,7 +21,7 @@ class folds_stats {
 		// instance = number of iterations to take avg over
 		const int instance = 10000;
 		// n = number of folds 
-		const int n = 44;
+		const int n = 42;
 
 		// Class constructor 
 		// seeds rng upon class instance creation
