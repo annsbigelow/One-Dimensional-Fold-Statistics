b94cde324a100f28cb13f1215850ef7a027decb9
annsbigelow <annsbigelow@gmail.com>
2025-01-15 19:14:44 -0600

    Update Num. Invariance

--- Numerical_Invariance.m
+++ Numerical_Invariance.m
@@ -7,7 +7,6 @@ h = 1/n;
 % Initial condition: constant function which integrates to 1
 f = 1 + zeros(1,n+1);
 grd=(0:h:1);
-disp(['Integral value:', num2str(trapz(grd,f))]);
 
 % Track the error
 steps = 4;
@@ -18,13 +17,22 @@ f_analytical = (48.* 0.9.*(9 + 2.*x - x.^2 - 2.* x.^3 + x.^4) + ...
 errvec = zeros(1,steps);
 
 % Iterate f
-steps = 10;
-for i = 1:steps
-    f = fNew(f,n,grd);
-    %f = f./trapz(f); % This normalization step shouldn't be included in our final version because it is cheating.
-    % Comment/uncomment line 16 to see the true integral value of f. 
-    disp(['Integral value:', num2str(trapz(grd,f))]);
-end
+%for m = 1:15
+    %Nvec(m)=n; % Increase n to check convergence
+    for i = 1:steps
+        f = fNew(f,n,grd);
+        %disp(['Integral value:', num2str(trapz(grd,f))]);
+        %errvec(i)=sqrt(h*sum((f_analytical-f).^2)); % Weighted norm
+        %errvec(i) = norm(f_analytical-f);
+    end
+%     errvec(m)=sqrt(h*sum((f_analytical-f).^2)); % Weighted norm
+%     n=n*2;
+%     h=1/n;
+%     f = 1 + zeros(1,n+1);
+%     grd=(0:h:1);
+%     x=grd;
+%     f_analytical = (48.* 0.9.*(9 + 2.*x - x.^2 - 2.* x.^3 + x.^4) + 1.25* (64 - 132.* x - 25.* x.^2 + 272.* x.^3 - 31.* x.^4 - 126.* x.^5 + 42.* x.^6))./(96.*(-2 + x).^2 .*(1 + x).^2);
+%end
 
 %Plot to check the final iteration
 plot(grd,f);
@@ -36,6 +44,26 @@ legend('numerical','analytical');
 hold off; 
 
 
+%[C,p]=my_loglog(Nvec,errvec,'n','Error','Error for Increasing Points');
+
+function [const,expo] = my_loglog(xin,yin,my_xlabel,my_ylabel,my_title)
+    % Fit a linear to error
+    pol = polyfit(log(xin), log(yin), 1);
+    const = exp(pol(2));
+    expo = pol(1);
+    
+    % Generate y values for the fitted line
+    yfit = polyval(pol, log(xin));
+    
+    % Plot 
+    loglog(xin, yin, 'o');
+    hold on;
+    %loglog(xin, exp(yfit), '-');
+    xlabel(my_xlabel);
+    ylabel(my_ylabel);
+    title(my_title);
+    hold off;
+end
 function z = L(x,f,n,grd) % Lagrange polynomial interpolant for each integration step
     % X,Y are sized k+1
     % Choose k points from 0 to 1 (not perfectly equally-spaced)
@@ -69,14 +97,9 @@ end
 
 
 function z = F(s,f,n,grd) 
-    % Trapezoid method for integration using Lagrange polynomials (this
-    %   expression was found via paper & pen). 
-    z = (1/2)*((1-s)/(2-s)*(L(s/(2-s),f,n,grd)/(2-s) + L(s,f,n,grd)) + s/(2*(2-s))*((L(s/2,f,n,grd)+L((2-s)/2,f,n,grd))/2 + (L(s/(2-s),f,n,grd)+f(end))/(2-s)));
-    
-    % MATLAB's "integral()"
-%     I1 = integral(@(t) t.*L(s.*t,f,n,grd), 1/(2-s), 1);
-%     I2 = integral(@(t) t.*(L(s.*t,f,n,grd) + L(t.*(2-s),f,n,grd)), 1/2, 1/(2-s));
-%     z = I1 + I2;
+    I1 = integral(@(t) t.*L(s.*t,f,n,grd), 1/(2-s), 1);
+    I2 = integral(@(t) t.*(L(s.*t,f,n,grd) + L(t.*(2-s),f,n,grd)), 1/2, 1/(2-s));
+    z = I1 + I2;
 end
 
 function fnew = fNew(f,n,grd)
@@ -86,3 +109,4 @@ function fnew = fNew(f,n,grd)
         fnew(i) = F(s,f,n,grd) + F(1-s,f,n,grd);
     end
 end
+
