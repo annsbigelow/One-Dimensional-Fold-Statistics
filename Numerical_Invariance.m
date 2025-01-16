69f7723c3b11b76acfe07725178bfef50fd94ceb
annsbigelow <annsbigelow@gmail.com>
2025-01-16 14:56:12 -0600

    Use Cheb pts in Num. Invariance

--- Numerical_Invariance.m
+++ Numerical_Invariance.m
@@ -1,12 +1,13 @@
 clear
 
 % Set up grid
-n = 80;
+n = 400;
 h = 1/n;
 
 % Initial condition: constant function which integrates to 1
 f = 1 + zeros(1,n+1);
-grd=(0:h:1);
+%grd=(0:h:1); % Equally spaced points
+grd=(1+cos((0:n)*pi./n))./2; % Chebyshev nodes for [0,1]
 
 % Track the error
 steps = 4;
@@ -17,22 +18,22 @@ f_analytical = (48.* 0.9.*(9 + 2.*x - x.^2 - 2.* x.^3 + x.^4) + ...
 errvec = zeros(1,steps);
 
 % Iterate f
-%for m = 1:15
+%for m = 1:10
     %Nvec(m)=n; % Increase n to check convergence
     for i = 1:steps
         f = fNew(f,n,grd);
-        %disp(['Integral value:', num2str(trapz(grd,f))]);
+        disp(['Approx. integral value:', num2str(trapz((0:h:1),f))]);
         %errvec(i)=sqrt(h*sum((f_analytical-f).^2)); % Weighted norm
-        %errvec(i) = norm(f_analytical-f);
     end
 %     errvec(m)=sqrt(h*sum((f_analytical-f).^2)); % Weighted norm
 %     n=n*2;
 %     h=1/n;
 %     f = 1 + zeros(1,n+1);
-%     grd=(0:h:1);
+%     %grd=(0:h:1);
+%     grd=(1+cos((0:n)*pi./n))./2; % Chebyshev nodes for [0,1]
 %     x=grd;
 %     f_analytical = (48.* 0.9.*(9 + 2.*x - x.^2 - 2.* x.^3 + x.^4) + 1.25* (64 - 132.* x - 25.* x.^2 + 272.* x.^3 - 31.* x.^4 - 126.* x.^5 + 42.* x.^6))./(96.*(-2 + x).^2 .*(1 + x).^2);
-%end
+% end
 
 %Plot to check the final iteration
 plot(grd,f);
@@ -44,10 +45,10 @@ legend('numerical','analytical');
 hold off; 
 
 
-%[C,p]=my_loglog(Nvec,errvec,'n','Error','Error for Increasing Points');
+%[C,p]=my_loglog(Nvec,errvec,'n','Error','Error over n, Cheb. nodes');
 
 function [const,expo] = my_loglog(xin,yin,my_xlabel,my_ylabel,my_title)
-    % Fit a linear to error
+    % Fit a line to error
     pol = polyfit(log(xin), log(yin), 1);
     const = exp(pol(2));
     expo = pol(1);
@@ -67,22 +68,22 @@ end
 function z = L(x,f,n,grd) % Lagrange polynomial interpolant for each integration step
     % X,Y are sized k+1
     % Choose k points from 0 to 1 (not perfectly equally-spaced)
-    k = 4; % Max degree
-    space = floor(n/k);
-    j = 1;
-    X = zeros(1,k+1);
-    Y = zeros(1,k+1);
-    for i = 1:k+1
-        X(i) = grd(j);
-        Y(i) = f(j); 
-        j = j + space;
-    end 
+%     k = 4; % Max degree
+%     space = floor(n/k);
+%     j = 1;
+%     X = zeros(1,k+1);
+%     Y = zeros(1,k+1);
+%     for i = 1:k+1
+%         X(i) = grd(j);
+%         Y(i) = f(j); 
+%         j = j + space;
+%     end 
 
-    % Lines 41-43 yield Runge Phenomenon (or choosing any more points than ~7)
-%     k=n;
-%     Y=f;
-%     X=grd;
-
+    k=n;
+    Y=f;
+    X=grd;
+    
+    % Interpolant construction
     z = 0;
     for j = 1:k+1
         prod = 1; 
