e5c0a72c1d8aae4f15992794ec2aa60f1a7adf1f
Chris Rycroft <chr@seas.harvard.edu>
2025-06-27 11:25:09 -0700

    Update ODE convergence test.

---
+++ crumple/brusselator.hh
@@ -0,0 +1,34 @@
+#ifndef BRUSSELATOR_HH
+#define BRUSSELATOR_HH
+
+#include "rk4.hh"
+
+#include <cstdio>
+
+/** This class has functions to specify the test Brusselator problem. */
+class brus_rk4 : public rk4 {
+    public:
+        brus_rk4() : rk4(2) {}
+        /** Evaluates the function f(x,y) on the RHS of the ODE.
+         * \param[in] t the dependent x variable in Hairer et al.'s notation.
+         * \param[in] in the array containing y variable.
+         * \param[in] out the function f(x,y). */
+        virtual void ff(double t_,double *in,double *out) {
+            double &y1=*in,&y2=in[1];
+            *out=1+y1*(y1*y2-4);
+            out[1]=y1*(3-y1*y2);
+        }
+        /** Sets up the initial conditions for the ODE. */
+        virtual void init() {
+            *q=1.5;
+            q[1]=3.;
+        }
+        virtual void print_step() {
+            printf("%g %g %g\n",t,*q,q[1]);
+        }
+        virtual void print_dense(double t_,double *in) {
+            printf("%g %g %g\n",t_,*in,in[1]);
+        }        
+};
+
+#endif
--- crumple/ode_conv.cc
+++ crumple/ode_conv.cc
@@ -1,4 +1,3 @@
-#include "sol_fsal.hh"
 #include "brusselator.hh"
 
 #include <cstdio>
@@ -6,41 +5,54 @@
 
 #include "omp.h"
 
+// The duration for the ODE convergence test
+const double dur=20.;
+
 int main() {
 
     // Compute reference solution
-//    brus_ckr br;
-    brus_fsal br;
-    br.solve(20.,3e-15,3e-15);
+    brus_rk4 br;
+    br.init();
+    br.solve_adaptive(dur,3e-15,3e-15);
     double ref0=br.q[0],ref1=br.q[1];
 
     // Allocate space for storing accuracy
-    const int N=105;
-    int fcount[N+1];
-    double err[N+1],t0=omp_get_wtime();
+    const int N=101;
+    int fcount[2*N];
+    double err[2*N],t0=omp_get_wtime();
 
     // Loop over a variety of grid sizes (given by i index) and integration
     // schemes (given by j index)
-#pragma omp parallel for schedule(dynamic)
-    for(int i=0;i<=N;i++) {
+#pragma omp parallel for schedule(dynamic) collapse(2)
+    for(int i=0;i<N;i++) for(int j=0;j<2;j++) {
 
         // Compute the number of steps according to a power law. Minimum
         // steps of 100, and maximum steps of 100000.
-//        brus_ckr *sb=new brus_ckr();
-        brus_fsal *sb=new brus_fsal();
+        brus_rk4 *sb=new brus_rk4();
+        sb->init();
 
         // Perform integration and compute the difference to the reference
         // solution
-        double tol=0.001*pow(0.1,0.1*i);
-        sb->solve(20.,tol,tol);
+        if(j==0) {
+            double tol=0.003*pow(0.1,0.1*i);
+            sb->solve_adaptive(dur,tol,tol);
+        } else sb->solve_fixed(dur,int(10*pow(10,0.03*i)));
         double dy0=ref0-sb->q[0],
                dy1=ref1-sb->q[1];
-        err[i]=sqrt(dy0*dy0+dy1*dy1);
-        fcount[i]=sb->fcount;
+        err[j*N+i]=sqrt(dy0*dy0+dy1*dy1);
+        fcount[j*N+i]=sb->fcount;
         delete sb;
     }
 
     // Output the results in a Gnuplot-readable format
     printf("# Time taken : %g ms\n",1e3*(omp_get_wtime()-t0));
-    for(int i=0;i<=N;i++) printf("%d %g\n",fcount[i],err[i]);
+    for(int j=0;j<2;j++) {
+        FILE *fp=fopen(j==0?"conv_adapt.dat":"conv_fixed.dat","w");
+        if(fp==NULL) {
+            fputs("Error opening file",stderr);
+            return 1;
+        }
+        for(int i=0;i<N;i++) fprintf(fp,"%d %g\n",fcount[j*N+i],err[j*N+i]);
+        fclose(fp);
+    }
 }
